24d3b31f64759639f0756df5c8883bcdda873d05
dokaptur <dokaptur@gmail.com>
2015-12-15 13:49:37 +0000

    problem 5 hopefully better

--- src/main/scala/uk/ac/ucl/cs/mr/statnlpbook/assignment2/Problem5.scala
+++ src/main/scala/uk/ac/ucl/cs/mr/statnlpbook/assignment2/Problem5.scala
@@ -103,38 +103,73 @@ case class JointConstrainedClassifier(triggerLabels:Set[Label],
                                       argumentFeature:(Candidate,Label)=>FeatureVector
                                        ) extends JointModel {
   def predict(x: Candidate, weights: Weights) = {
+    // get best label for given candidate and it's score
     def argmax(labels: Set[Label], x: Candidate, weights: Weights, feat:(Candidate,Label)=>FeatureVector) = {
       val scores = labels.toSeq.map(y => y -> dot(feat(x, y), weights)).toMap withDefaultValue 0.0
-      scores.maxBy(_._2)._1
+      scores.maxBy(_._2)
     }
-    def argmaxTheme(x:Candidate, weights: Weights, feat:(Candidate,Label)=>FeatureVector) = {
-      val scores = x.arguments.map(l => l -> dot(feat(l, new Label("Theme")), weights)).toMap withDefaultValue 0.0
-      scores.maxBy(_._2)._1
+    // get all scores for trigger
+    def triggerScores(labels: Set[Label], x: Candidate, weights: Weights, feat:(Candidate,Label)=>FeatureVector) = {
+      val scores = labels.toSeq.map(y => y -> dot(feat(x, y), weights)).toMap withDefaultValue 0.0
+      //scores.toSeq.sortBy(- _._2) // best results first
+      scores
     }
-    val bestTrigger = argmax(triggerLabels,x,weights,triggerFeature)
+    // get argument with the best result for "Theme" (and it's result)
+    def bestThemeArg(x:Candidate, weights: Weights, feat:(Candidate,Label)=>FeatureVector) = {
+      val scores = x.arguments.map(l => l -> dot(feat(l, "Theme"), weights)).toMap withDefaultValue 0.0
+      scores.maxBy(_._2)
+    }
 
-    if (bestTrigger==new Label("None"))
-    {
-      val bestArguments = for (arg <- x.arguments) yield new Label("None")
-      (bestTrigger,bestArguments)
+    def reproduceArgLabels(argumentSeq: Seq[Candidate], labelsMap: Map[Candidate, Label], themeArg: Candidate) = {
+      val argLabels = for (a <- argumentSeq ) yield {
+        labelsMap.getOrElse(a, "Theme")
+      }
+      argLabels
     }
-    else if (bestTrigger!=new Label("Regulation")){
 
-      val firstThemeArguments = argmaxTheme(x, weights, argumentFeature)
-      argumentLabels.-(new Label ("Cause"))
-      val bestArguments = for (arg <- x.arguments) yield
-      {if (arg==firstThemeArguments) {new Label("Theme")}
-      else argmax(argumentLabels, arg, weights, argumentFeature)}
-      (bestTrigger,bestArguments)
-    }
-    else {
-      val firstThemeArguments = argmaxTheme(x, weights, argumentFeature)
-      //argumentLabels.-(new Label ("Cause"))
-      val bestArguments = for (arg <- x.arguments) yield
-      {if (arg==firstThemeArguments) {new Label("Theme")}
-      else argmax(argumentLabels, arg, weights, argumentFeature)}
-      (bestTrigger,bestArguments)
-    }
+    // get sum of scores for all arguments labeled "None"
+    val noneArgsScore = x.arguments.map(a => argmax(argumentLabels, a, weights, argumentFeature)._2).sum
+    // get the best argument to be forced to be "Theme" and it's score
+    val (themeArg, themeArgScore) = bestThemeArg(x, weights, argumentFeature)
+    // exclude "Theme" argument from arguments
+    val argsNoTheme = x.arguments.filter(a => !a.equals(themeArg))
+    //exclude "Cause" from Argument Labels
+    val argLabelsNoCause = argumentLabels.filter(l => !l.equals("Cause"))
+    // get set of all regulation trigger Labels
+    val regulationTriggerLabels = Set("Regulation")
+
+    //find best labels for rest of arguments, sum score
+    val bestArgLabels = argsNoTheme.map(a => a -> argmax(argumentLabels, a, weights, argumentFeature))
+    val bestArgLabelsScore = bestArgLabels.map(a => a._2._2).sum
+
+    // find best labels (excluding "Cause") for rest of arguments, sum score
+    val bestArgLabelsNoCause = argsNoTheme.map(a => a -> argmax(argLabelsNoCause, a, weights, argumentFeature))
+    val bestArgLabelsNoCauseScore = bestArgLabelsNoCause.map(a => a._2._2).sum
+
+    // get scores for all trigger labels
+    val triggerResults = triggerScores(triggerLabels, x, weights, triggerFeature)
+
+    // find the best combinations and save them to map
+    val scoresMap = new mutable.HashMap[(Label, Seq[Label]), Double]()
+
+    // get results for None
+    val noneArgLabels = for (a <- x.arguments) yield "None"
+    scoresMap += ("None", noneArgLabels) -> (triggerResults("None") + noneArgsScore)
+
+    // get results for event different than None
+    val (bestNonNoneTriggerLabel, bestNonNoneTriggerScore) = triggerResults.filter(!_._1.equals("None")).maxBy(_._2)
+    val nonNoneScore = bestNonNoneTriggerScore + themeArgScore + bestArgLabelsNoCauseScore
+    scoresMap += (bestNonNoneTriggerLabel,
+      reproduceArgLabels(argsNoTheme, bestArgLabelsNoCause.map(a => a._1 -> a._2._1).toMap, themeArg)) -> nonNoneScore
+
+    // get results for regulation event
+    val (bestRegulationTriggerLabel, bestRegulationTriggerScore) =
+      triggerResults.filter(l => regulationTriggerLabels.contains(l._1)).maxBy(_._2)
+    val regulationScore = bestRegulationTriggerScore + themeArgScore + bestArgLabelsScore
+    scoresMap += (bestRegulationTriggerLabel,
+      reproduceArgLabels(argsNoTheme, bestArgLabels.map(a => a._1 -> a._2._1).toMap, themeArg)) -> regulationScore
+
+    scoresMap.toMap.maxBy(_._2)._1
 
   }
 
