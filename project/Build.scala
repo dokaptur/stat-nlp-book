173d319b83d6b02b4bc0795ff591eccff23d1b50
dokaptur <dokaptur@gmail.com>
2016-01-15 10:22:24 +0000

    a3 setup

--- project/Build.scala
+++ project/Build.scala
@@ -4,7 +4,8 @@ import sbt.Keys._
 object BuildSettings {
   val buildName = "statnlpbook"
   val buildOrganization = "uk.ac.ucl.cs.mr"
-  val buildScalaVersion = "2.11.4"
+  val buildScalaVersion = "2.11.7"
+
 
   val buildSettings = Defaults.defaultSettings ++ Seq(
     organization := buildOrganization,
@@ -16,7 +17,17 @@ object BuildSettings {
       "org.scala-lang.modules" %% "scala-pickling" % "0.10.0",
       "ml.wolfe" %% "wolfe-core" % "0.6.0",
       "ml.wolfe" %% "wolfe-ui" % "0.6.0",
-      "ml.wolfe" %% "wolfe-nlp" % "0.6.0"),
+      "ml.wolfe" %% "wolfe-nlp" % "0.6.0",
+      "org.scalanlp" %% "breeze" % "0.11.2",
+      // native libraries are not included by default. add this if you want them (as of 0.7)
+      // native libraries greatly improve performance, but increase jar sizes.
+      // It also packages various blas implementations, which have licenses that may or may not
+      // be compatible with the Apache License. No GPL code, as best I know.
+      "org.scalanlp" %% "breeze-natives" % "0.11.2",
+      "com.github.tototoshi" %% "scala-csv" % "1.3.0-SNAPSHOT",
+      "org.scalatest" % "scalatest_2.11" % "2.2.4" % "test"
+    ),
+
 
 
   //shellPrompt := ShellPrompt.buildShellPrompt,
@@ -24,10 +35,10 @@ object BuildSettings {
     connectInput in run := true, //to use readLine after sbt run
     commands ++= Seq(vmargs),
     resolvers ++= Seq(
-      "IESL Release" at "https://dev-iesl.cs.umass.edu/nexus/content/groups/public",
-      "UCLMR Release" at "http://homeniscient.cs.ucl.ac.uk:8081/nexus/content/repositories/releases"
-    ),
-    addCompilerPlugin("org.scalamacros" % "paradise" % "2.1.0-M1" cross CrossVersion.full)
+      "Sonatype Snapshots" at "https://oss.sonatype.org/content/repositories/snapshots/",
+      "Sonatype Releases" at "https://oss.sonatype.org/content/repositories/releases/"
+    )
+    //addCompilerPlugin("org.scalamacros" % "paradise" % "2.1.0-M1" cross CrossVersion.full)
   )
 
   val generalSettings =
